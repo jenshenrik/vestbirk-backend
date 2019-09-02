21162bd3446246c5a1f07e079f2828d83912502b
Jens Henrik Skuldbøl Vogelius <jenshenrik.skuldbol@gmail.com>
2019-09-02 12:56:55 +0200

    Copied guild and contact person models, and admin config

--- vestbirk/src/guilds/admin.py
+++ vestbirk/src/guilds/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Guild, ContactPerson
+
+class GuildAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Content', { 'fields': ['title', 'short_text', 'text']}),
+        ('Contact Information', {'fields': ['homepage_url', 'facebook_url', 'contact_person']})
+    ]
 
 # Register your models here.
+admin.site.register(Guild, GuildAdmin)
+admin.site.register(ContactPerson)
--- vestbirk/src/guilds/models.py
+++ vestbirk/src/guilds/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class ContactPerson(models.Model):
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    PhoneNumber = models.CharField(null=True, blank=True, max_length=12)
+    Email = models.CharField(null=True, blank=True, max_length=100)
+
+    def __str__(self):
+        return self.FirstName + self.LastName
+
+
+class Guild(models.Model):
+    title = models.CharField(max_length=200)
+    short_text = models.TextField(max_length=500)
+    text = models.TextField()
+
+    homepage_url = models.CharField(max_length=500, blank=True)
+    facebook_url = models.CharField(max_length=500, blank=True)
+    contact_person = models.ForeignKey(ContactPerson, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
+
+
