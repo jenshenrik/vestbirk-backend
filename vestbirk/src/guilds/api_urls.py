1d1e7d90930182f20e0f5e76308b78d651b6a396
Jens Henrik Skuldbøl Vogelius <jenshenrik.skuldbol@gmail.com>
2019-09-02 15:11:02 +0200

    Started setting up urls

---
+++ vestbirk/src/guilds/api_urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from guilds.views import index, guild_by_id
+
+urlpatterns = [
+    path('/', index, name='all_guilds'),
+    path('/<int:id>', guild_by_id),
+]
--- vestbirk/src/guilds/views.py
+++ vestbirk/src/guilds/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+#from vestbirk.guilds.models import Guild
+
 # Create your views here.
+
+def index(request):
+    return json_response('Hello world!')
+    #lguilds = Guild.objects.all()
+    #return json_response(guilds)
+
+def guild_by_id(request, id):
+    return json_response(id)
