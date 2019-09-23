b1e091361c2eeda2284575f542a0e348f29e0c8c
Jens Henrik Skuldbøl Vogelius <jenshenrik.skuldbol@gmail.com>
2019-09-23 19:40:33 +0200

    Fixed CORS

--- vestbirk/src/vestbirk/settings.py
+++ vestbirk/src/vestbirk/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'tinymce',
     'guilds',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,6 +122,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
